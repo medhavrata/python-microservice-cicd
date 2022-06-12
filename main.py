aafcf425f51dfb91eaea1c170a9887073a1f9ce8
Medha <tyagi.25@gmail.com>
2022-06-12 23:18:42 +0000

    created fastapi

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
 
-result = wiki()
-print(result)
+from mylib.logic import wiki
+from mylib.logic import search_wiki
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API for search or wiki"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """search wikipedia"""
+
+    search_result = search_wiki(value)
+    return {"result": search_result}
+
+@app.get("/wiki/{value}")
+async def wiki_get(value: str):
+    """get value from wiki"""
+
+    get_wiki = wiki(value)
+    return {"wiki_value": get_wiki}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
